b798568c0e145178e3373151f9a7df45171baab2
F-Yousri <fahdyousri@gmail.com>
2018-10-09 23:27:31 +0200

    fix dependent typo

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,5 @@
 class Choice < ApplicationRecord
-    has_many :pump_property, dependant: :destroy
+    has_many :pump_property, dependent: :destroy
     belongs_to :property
     has_many :pumps, through: :Pump_property
     
