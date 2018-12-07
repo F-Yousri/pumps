56f912e2c24a1d0a10c7baabe7465a042a92caa4
minaibrahimsaid <minaibrahimsaid1991@gmail.com>
2018-12-07 20:22:59 +0200

    hot fix 3

--- app/admin/sucker_rods.rb
+++ app/admin/sucker_rods.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register SuckerRod do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :api, :yield_strength, :Weatherford, :corrosion_resistance
 #
 # or
 #
