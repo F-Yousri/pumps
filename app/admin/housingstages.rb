32fbbff1a3f211f68dd48582962517328bd235ef
minaibrahimsaid <minaibrahimsaid1991@gmail.com>
2018-12-14 21:17:54 +0200

    hotfix

--- app/admin/housingstages.rb
+++ app/admin/housingstages.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Housingstage do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :housing, :stages, :cost, :cost
+permit_params :housing, :stages, :cost, :type
 #
 # or
 #
