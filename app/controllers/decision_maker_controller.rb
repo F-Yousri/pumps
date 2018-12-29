621596eb3d25a3f98123024802f998e0946aa863
minaibrahimsaid <minaibrahimsaid1991@gmail.com>
2018-12-29 17:27:14 +0200

    pump 1

--- app/controllers/decision_maker_controller.rb
+++ app/controllers/decision_maker_controller.rb
@@ -3,19 +3,22 @@ class DecisionMakerController < ApplicationController
     Dir["#{Rails.root}/app/services/phasetwo/*.rb"].each {|file| require file }
 
 
-    skip_before_action :verify_authenticity_token  
-
+    skip_before_action :verify_authenticity_token
+    $phaseoneparams
+    
     def techEvalForm        
     end
 
     def techEval
         session[:params] ||= params
+        $phaseoneparams=params
         @params = DecisionMakerService.make(params)
-        render json:  params
+        render json:$phaseoneparams
+        # render  template: 'resultphaseone' , locals: { pumps: @params }
     end
 
     def phaseTwoPump1
-        @mina=PhaseTwoPumpOne.pumpone(params)
+        @mina=PhaseTwoPumpOne.pumpone(params,$phaseoneparams)
         render json:@mina
     end
 
--- app/services/phasetwo/phasetwopumpone.rb
+++ app/services/phasetwo/phasetwopumpone.rb
@@ -3,43 +3,49 @@ module PhaseTwoPumpOne
     class << self
 
 
-        def pumpone params
-            @SR_ND=params[:SR_ND]
-            @SR1_ND=1
+        def pumpone (params,phaseoneparams)
+            @SR_ND=params[:SR_ND].to_f
+            @SR1_ND=1  #mina
             @RT=params[:RT]
             data = TableService.new(Tablegenerate.new('SuckerRodTable').get_table,@RT).final
             @YS_min=data[0]['yield_strength']
-            @SL=params[:SL]
-            @N_SRP=params[:N_SRP]
-            @ID_SRP=2.155
+            @SL=params[:SL].to_f
+            @N_SRP=params[:N_SRP].to_f
+            @ID_SRP=Math.sqrt(phaseoneparams[:GQ].to_f/(0.1166*@SL*@N_SRP))
             @ID_p = TableService.new(Tablegenerate.new('AvailableSuckerRodPumpSize').get_table,@ID_SRP).final
             @array=TableService.new(Tablegenerate.new('RodStringTaperingPercentagesTable').get_table,{ID_p: @ID_p,SR_ND: @SR_ND,pump: 2}).final
-            @R1= @array[0]['size_118']
-            @R2=@array[0]['size_1']
-            @R3=@array[0]['size_78']
-            @R4=@array[0]['size_34']
+            @R1= @array[0]['size_118'] 
+            @R2=@array[0]['size_1']  
+            @R3=@array[0]['size_78'] 
+            @R4=@array[0]['size_34']  
             @SW_r=@array[0]['Rod_Weight']
-            @PEff_srp=params[:PEff_srp]
-            @MEff_srp=params[:MEff_srp]
-            @L_p=5
-            @L_spacing=54
-            @L_b=19.5
+            @PEff_srp=params[:PEff_srp].to_f
+            @MEff_srp=params[:MEff_srp].to_f
+            @L_p=5 #mina
+            @L_spacing=54 #mina
+            @L_b=(@SL+(@L_p*12)+@L_spacing)/12
             @L_bs = TableService.new(Tablegenerate.new('BarrelSizesTable').get_table,@L_b).final
-            @Delta=0.170
-            @W_r=8585.500
-            @Fo=6093.600
-            @PPRL=16140.462
-            @S_axial=20550.674
-            @MPRL=6012.820
-            @PT=303829.271
+            @Delta=@SL*(@N_SRP**2)/70500
+            @W_r=@SW_r*phaseoneparams[:VD_pump].to_f
+            @Fo=phaseoneparams[:WGD].to_f*phaseoneparams[:VD_pump].to_f*(Math::PI/4*@ID_p**2)*phaseoneparams[:SG_m].to_f #(pi()/4*ID_p^2)*SG_m  =C28*C20*(PI()/4*H11^2)*C61
+            @PPRL=@Fo+@W_r*(1+@Delta)
+            @S_axial=@PPRL/(Math::PI/(4*@SR1_ND**2))
+            @MPRL=@W_r*(1-0.128**phaseoneparams[:SG_m].to_f-@Delta)
+            @PT=@SL/4*(@PPRL-@MPRL)
             @data = TableService.new(Tablegenerate.new('AvailablePumpingUnitTable').get_table,{PT: @PT,PPRL: @PPRL}).final
             @PPRL100=@data[:PPRL100]
             @PT1000=@data[:PT1000]
             @SL=@data[:SL]
-            @MHP_srp=64.468
+            @HHP_srp=phaseoneparams[:GQ].to_f*phaseoneparams[:VD_pump].to_f/56000
+            @FHP_srp=0.000000631*@W_r*@SL*@N_SRP
+            @PEff_srp=params[:PEff_srp].to_f
+            @MEff_srp=params[:MEff_srp].to_f
+            @MHP_srp=(@HHP_srp+@FHP_srp)/(@PEff_srp*@MEff_srp)
             @L_bs = TableService.new(Tablegenerate.new('NemaTable').get_table,@MHP_srp).final
-            @L_bs
+            @EC_srp=0.746*@MHP_srp*24*365*phaseoneparams[:EC].to_f
         end
         
     end
 end
+
+#mina   SG_m is given from phase one  ?
