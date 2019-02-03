ca821d55b792bd4653f977d706bd7f3e1f1e25d6
minaibrahimsaid <minaibrahimsaid1991@gmail.com>
2019-02-03 19:03:35 +0200

    edit key

--- app/models/phasethree/tables/RodStringPriceTable.rb
+++ app/models/phasethree/tables/RodStringPriceTable.rb
@@ -8,13 +8,13 @@ class RodStringPriceTable
     def get_data input
         input=input.downcase
         @pn6=RodStringPrice.select("#{input}").where(size: "6")
-        @pn6=@pn6[0]['d']
+        @pn6=@pn6[0][input]
         @pn7=RodStringPrice.select("#{input}").where(size: "7")
-        @pn7=@pn7[0]['d']
+        @pn7=@pn7[0][input]
         @pn8=RodStringPrice.select("#{input}").where(size: "8")
-        @pn8=@pn8[0]['d']
+        @pn8=@pn8[0][input]
         @pn9=RodStringPrice.select("#{input}").where(size: "9")
-        @pn9=@pn9[0]['d']
+        @pn9=@pn9[0][input]
         {
             pn6:@pn6,
             pn7:@pn7,
