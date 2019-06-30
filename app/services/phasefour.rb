cdbc54fa10e1e21a08d0093ede4e81623aa5c03e
minaibrahimsaid <minaibrahim1991@yahoo.com>
2019-06-30 13:36:43 -0400

    xi

--- app/services/phasefour.rb
+++ app/services/phasefour.rb
@@ -3,7 +3,7 @@ module PhaseFour
         def make (phaseoneresult,pump,resultphasthree) 
             finalPumps = getonlyrightpump(phaseoneresult ,pump ) 
             cdcpumps = addcdc(finalPumps , resultphasthree)   
-            xiarray  = clacxi (cdcpumps)
+            # xiarray  = clacxi (cdcpumps)
         end
 
 
@@ -37,33 +37,33 @@ module PhaseFour
 
         def clacxi cdcpumps
             xiarray={}
-            xiwl=ximd_pump=0
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  ximd_pump =ximd_pump +  pump[1]['MD_pump'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            cdcpumps.each{ |pump|  xiwl =xiwl +  pump[1]['WL'][0].to_f**2  }
-            xiarray[:xiwl] = Math.sqrt(xiwl)
-            xiarray[:ximd_pump] = Math.sqrt(ximd_pump)
+            wl=md_pump=wd=csg_nd=ds=gq=j=t_bh=meo_m=api=api=cp
+            cdcpumps.each{ |pump|  wl =wl +  pump[1]['WL'][0].to_f**2  }
+            cdcpumps.each{ |pump|  md_pump =md_pump +  pump[1]['MD_pump'][0].to_f**2  }
+            cdcpumps.each{ |pump|  wd =wd +  pump[1]['WD'][0].to_f**2  }
+            cdcpumps.each{ |pump|  csg_nd =csg_nd +  pump[1]['CSG_ND'][0].to_f**2  }
+            cdcpumps.each{ |pump|  ds =ds +  pump[1]['DS'][0].to_f**2  }
+            cdcpumps.each{ |pump|  gq =gq +  pump[1]['GQ'][0].to_f**2  }
+            cdcpumps.each{ |pump|  j =j +  pump[1]['J'][0].to_f**2  }
+            cdcpumps.each{ |pump|  t_bh =t_bh +  pump[1]['T_bh'][0].to_f**2  }
+            cdcpumps.each{ |pump|  meo_m =meo_m +  pump[1]['meo_m'][0].to_f**2  }
+            cdcpumps.each{ |pump|  api =api +  pump[1]['API'][0].to_f**2  }
+            cdcpumps.each{ |pump|  ap =ap +  pump[1]['AP'][0].to_f**2  }
+            cdcpumps.each{ |pump|  cp =cp +  pump[1]['CP'][0].to_f**2  }
+
+            xiarray[:wl] = Math.sqrt(wl)
+            xiarray[:wd] = Math.sqrt(wd)
+            xiarray[:csg_nd] = Math.sqrt(csg_nd)
+            xiarray[:ds] = Math.sqrt(ds)
+            xiarray[:gq] = Math.sqrt(gq)
+            xiarray[:j] = Math.sqrt(j)
+            xiarray[:t_bh] = Math.sqrt(t_bh)
+            xiarray[:meo_m] = Math.sqrt(meo_m)
+            xiarray[:api] = Math.sqrt(api)
+            xiarray[:ap] = Math.sqrt(ap)
+            xiarray[:cp] = Math.sqrt(cp)
+
+
             xiarray
         end
     end
